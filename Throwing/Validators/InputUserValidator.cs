546647e14f556500c669c3d1a5ba3897498e4ae2
f0urth <f0urth.kamil@gmial.com>
2022-08-23 00:04:26 +0200

    Correct base

---
+++ Throwing/Validators/InputUserValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace NotThrowing.Validators;
+
+using System.Text.RegularExpressions;
+using FluentValidation;
+using Models;
+
+public class InputUserValidator : AbstractValidator<InputUser>
+{
+    public InputUserValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+        
+        RuleFor(e => e.FullName)
+            .NotEmpty()
+            .Matches("^[a-z0-9 ,.'-]$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        RuleFor(e => e.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(e => e.GithubUsername).NotEmpty();
+    }
+}
